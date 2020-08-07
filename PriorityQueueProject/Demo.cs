d9d5e1943661be085e9f9ffdfd5e2571bb438df8
Poojitha Pancheti <Poojitha.Pancheti@DESKTOP-UK1T43N>
2020-08-07 16:53:46 +0530

    Add project files.

---
+++ PriorityQueueProject/Demo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriorityQueueProject
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            int choice, element, elementPriority;
+
+            PriorityQueueL pq = new PriorityQueueL();
+
+            while (true)
+            {
+                Console.WriteLine("1.Insert a new  element ");
+                Console.WriteLine("2.Delete an element ");
+                Console.WriteLine("3.Display a queue ");
+                Console.WriteLine("4.Quit ");
+                Console.WriteLine("Enter  your choice : ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 4)
+                    break;
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the element to be inserted ");
+                        element = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the priority : ");
+                        elementPriority = Convert.ToInt32(Console.ReadLine());
+                        pq.Insert(element, elementPriority);
+                        break;
+                    case 2:
+                        Console.WriteLine("Deleted Element is : " + pq.Delete());
+                        break;
+                    case 3:
+                        pq.Display();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice");
+                        break;
+
+                }
+
+            }
+        }
+    }
+}
---
+++ PriorityQueueProject/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriorityQueueProject
+{
+    class Node
+    {
+        public int priority;
+        public int info;
+        public Node link;
+
+        public Node(int i, int pr)
+        {
+            info = i;
+            priority = pr;
+            link = null;
+        }
+    }
+}
